08e52bfc7c885f4f87b53264d151d2c9a7ef3927
ErwanR-Holberton <6218@holbertonstudents.com>
2024-02-05 15:27:42 +0100

    Added filter on button

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -43,4 +43,33 @@ $(document).ready(function() {
             console.error('Error submitting data:', error);
         }
     });
+
+    $('button').click(function() {
+        console.log("click button");
+        $.ajax({
+            url: 'http://127.0.0.1:5001/api/v1/places_search/',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({amenities: selectedAmenities}),
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+                $('.places').empty();
+                for (let place of data) {
+                    let article = $("<article>").appendTo(".places");
+                    let title = $("<div>").addClass("title_box").appendTo(article);
+                    $("<h2>").text(place.name).appendTo(title);
+                    $("<div>").addClass("price_by_night").text(place.price_by_night + "$").appendTo(title);
+                    let info = $("<div>").addClass("information").appendTo(article);
+                    $("<div>").addClass("max_guest").text(place.max_guest).appendTo(info);
+                    $("<div>").addClass("number_rooms").text(place.number_rooms).appendTo(info);
+                    $("<div>").addClass("number_bathrooms").text(place.number_bathrooms).appendTo(info);
+                    let description = $("<div>").addClass("description").text(place.description).appendTo(article);
+                }
+            },
+            error: function(error) {
+                console.error('Error submitting data:', error);
+            }
+        });
+    });
 });
