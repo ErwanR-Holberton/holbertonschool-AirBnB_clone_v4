256fd1c5a2e616bacf0729e461e621bb65a70bc1
ErwanR-Holberton <6218@holbertonstudents.com>
2024-02-05 14:08:04 +0100

    Added api status at top right

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    let selectedAmenities = {};
+    $('input[type="checkbox"]').change(function() {
+        let amenityId = $(this).data('id');
+        let amenityName = $(this).data('name');
+        if ($(this).prop('checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+        let selectedAmenitiesText = Object.values(selectedAmenities).join(', ');
+        $('.amenities h4').text(selectedAmenitiesText);
+    });
+
+    $.get("http://127.0.0.1:5001/api/v1/status/", function(data, textStatus)
+    {
+        if (data.status === "OK") {
+            $('#api_status').addClass("available");
+            console.log("added class");
+        } else {
+            $('#api_status').removeClass("available");
+            console.log("rm class");
+        }
+    });
+});
